a98bb3e38af58a1273ca694481cffe0b9840a3ce
Ezzk0 <147737162+Ezzk0@users.noreply.github.com>
2023-10-12 18:13:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function print_thx() {
+    alert("Спасибо!")
+}
+
+function print_help() {
+    prompt("Опишите свою проблему")
+}
+
+function print_gamelink() {
+    alert("Перейдите по ссылке: https://www.minecraft.net/ru-ru")
+}
